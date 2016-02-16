172d3830d0e29f44215ef42f717598ed417277be
rchatters <chatters@gmail.com>
2016-02-15 22:51:41 -0500

    feat: add config data object

--- server.js
+++ server.js
@@ -1,36 +1,47 @@
 var express = require('express');
 var fs = require('fs');
 var request = require('request');
-var cheerio = require('cheerio');
-var app = express();
+var cheerio 	= require('cheerio');
+var app 	    = express();
+var fs 			= require('fs');
+var path 		= require('path');
 
 var minutes = 1;
 var the_interval = minutes * 60 * 1000;
 
 app.set('port', (process.env.PORT || 8081));
 
-app.get('/scrape', function(req, res) {
-	var url = 'http://mailchimp.com/replyall/';
-	var isAvailable = false;
+var data = JSON.parse(fs.readFileSync(path.join(__dirname + '/data.json'),'utf-8'));
+var self = this;
+app.get('/', function(req, res) {
+	data = data || null;
+	if (data) {
+	  var isAvailable = false;
+	  var notifed = false;
+	  setInterval(function() {
+		for (var i = 0; i< data.checker.length; i++) {
+			var url = data.checker[i].url;
+			request(url, function(error, response, html) {
+					if(!error) {
+						var $ = cheerio.load(html);
+						isAvailable = $('button').hasClass('giveaway');
+					}
+					if(isAvailable && !notifed) {
+						//send email
+						notifed = true;
+					}
+					else {
+						notifed = false;
+					}
 
-	//setInterval(function() {
-	request(url, function(error, response, html) {
-
-		if(!error) {
-			var $ = cheerio.load(html);
-			isAvailable = $('button').hasClass('giveaway');
+					res.send(isAvailable);
+				});
 		}
-		console.log(isAvailable);
-		if(isAvailable) {
-
-		}
-
-		res.send(isAvailable);
-	})
- 	//}, the_interval);
+ 	}, the_interval);
+	}
 });
 
 app.listen(app.get('port'), function() {
-	console.log('Magic happens on port 8081');
+	console.log('Magic happens on port', app.get('port'));
 });
 exports = module.exports = app;
